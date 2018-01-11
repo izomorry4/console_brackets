7d560348b85cf76cc5e4ebe93311c59390c63d90
izomorry4 <izomorry4@yandex.ru>
2018-01-12 00:37:31 +0300

    console_brackets

--- script.php
+++ script.php
@@ -7,9 +7,19 @@
  * Time: 21:24
  */
 
+require 'vendor/autoload.php';
+
 $file = '';
 while (!$file) {
     echo "Enter file with brackets: ";
     $file = trim(fgets(STDIN));
 }
-echo file_get_contents($file);
+$str = file_get_contents($file);
+
+try {
+    BracketChecker::IsCorrect($str);
+}
+catch(Exception $e){
+    $e->getMessage();
+}
+
